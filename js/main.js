65ecaed26ba85e2b36920c14d2a1b9d41e3e72ce
marcosarango <sar21005@byui.edu>
2022-01-08 14:15:18 -0500

    update

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
 const links = [
-    {label: "Week1", URL: "/week1/index.html"},
+    {
+        label: "Week1", 
+        url: ""
+    },
 ]
-const list = document.getElementById("weekList");
-for (let i = 0; i< links.length; i++){
-    let li = document.createElement("li");
-    let label = links[i].label;
-    let URL = links[i].URL;
-    li.innerHTML = "<a href=" + URL + ">" + label + "</a>";
-    list.appendChild(li);
-    
-}
+
+const list = document.getElementById('weekList');
+
+for (let i=0; i<links.length; i++){
+  // create an element <li> <a></a> </li> inside <ol></ol>
+  let li = document.createElement('li');
+  let a  = document.createElement('a');
+
+  a.setAttribute('href', links[i].url); //set a link/url for Week[i]
+  a.textContent = links[i].label + " : " + links[i].notes;  // set a Label of Week[i]
+
+  li.appendChild(a); // put the a inside the li
+  list.appendChild(li); // put the li inside the #list
+};
+
+  console.log("hello world!")
