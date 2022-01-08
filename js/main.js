265d46d1226fa92cb26b401290807d1ac1ee23d3
marcosarango <sar21005@byui.edu>
2022-01-08 00:08:37 -0500

    update week

--- js/main.js
+++ js/main.js
@@ -2,3 +2,11 @@ const links = [
     {label: "Week1", URL: "/week1/index.html"},
 ]
 const list = document.getElementById("weekList");
+for (let i = 0; i< links.length; i++){
+    let li = document.createElement("li");
+    let label = links[i].label;
+    let URL = links[i].URL;
+    li.innerHTML = "<a href=" + URL + ">" + label + "</a>";
+    list.appendChild(li);
+    
+}
