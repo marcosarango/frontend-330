fad4484400dc4a19ec3f72720be5f8101a566ffc
marcosarango <sar21005@byui.edu>
2022-01-08 16:13:45 -0500

    update

--- js/main.js
+++ js/main.js
@@ -1,22 +1,17 @@
 const links = [
-    {
-        label: "Week1", 
-        url: ""
-    },
-]
+  { label: "Week 1 notes", url: "week1/index.html" },
+  { label: "Week 2 notes", url: "week2/index.html" },
+  { label: "Week 3 notes", url: "week3/index.html" },
+  { label: "Week 4 notes", url: "week4/index.html" },
+ 
+];
 
-const list = document.getElementById('weekList');
+let list = document.getElementById("list");
 
-for (let i=0; i<links.length; i++){
-  // create an element <li> <a></a> </li> inside <ol></ol>
-  let li = document.createElement('li');
-  let a  = document.createElement('a');
-
-  a.setAttribute('href', links[i].url); //set a link/url for Week[i]
-  a.textContent = links[i].label + " : " + links[i].notes;  // set a Label of Week[i]
-
-  li.appendChild(a); // put the a inside the li
-  list.appendChild(li); // put the li inside the #list
-};
-
-  console.log("hello world!")
+for (let i = 0; i < links.length; i++) {
+  let li = document.createElement("li");
+  let label = links[i].label;
+  let url = links[i].url;
+  li.innerHTML = "<a href='" + url + "'>" + label + "</a>";
+  list.appendChild(li);
+}
