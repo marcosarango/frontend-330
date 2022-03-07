acce89f9cdee744fed08ce5d4c49087a973429d2
sar21005@byui.edu <sar21005@byui.edu>
2022-03-07 17:32:07 -0500

    week9

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ const links = [
   { label: "Week 6 notes", url: "project/index.html" },
   { label: "Week 7 notes", url: "week7/index.html" },
   { label: "Week 8 notes", url: "week8/index.html" },
+  { label: "Week 9 notes", url: "week9/index.html" },
  
 ];
 
